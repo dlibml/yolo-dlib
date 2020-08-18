329f8ce53e4ccbfecd1cfcf7cfbe428213b5c0a4
Adrià Arrufat <adria.arrufat@outlook.com>
2020-08-18 17:23:33 +0900

    use a darknet backbone as a start, try to minimize differences

---
+++ src/darknet.h
@@ -0,0 +1,108 @@
+#ifndef darknet_h_INCLUDED
+#define darknet_h_INCLUDED
+
+#include "yolo.h"
+
+#include <dlib/dnn.h>
+
+namespace darknet
+{
+    // clang-format off
+    using namespace dlib;
+
+    template <template <typename> class BN, template <typename> class ACT>
+    struct def
+    {
+        template <long num_filters, int kernel_size, int stride, int padding, typename SUBNET>
+        using convolutional = ACT<BN<add_layer<
+            con_<num_filters, kernel_size, kernel_size, stride, stride, padding, padding>,
+            SUBNET>>>;
+
+        template <long num_filters, typename SUBNET>
+        using residual = add_prev1<
+            convolutional<num_filters, 3, 1, 1,
+            convolutional<num_filters / 2, 1, 1, 0,
+            tag1<SUBNET>>>>;
+
+        template <long num_filters, typename SUBNET>
+        using block3 = 
+            convolutional<num_filters, 3, 1, 1,
+            convolutional<num_filters / 2, 1, 1, 0,
+            convolutional<num_filters, 3, 1, 1,
+            SUBNET>>>;
+
+        template <long num_filters, typename SUBNET>
+        using block5 = 
+            convolutional<num_filters, 3, 1, 1,
+            convolutional<num_filters / 2, 1, 1, 0,
+            convolutional<num_filters, 3, 1, 1,
+            convolutional<num_filters / 2, 1, 1, 0,
+            convolutional<num_filters, 3, 1, 1,
+            SUBNET>>>>>;
+
+        template <typename SUBNET> using residual_128 = residual<128, SUBNET>;
+        template <typename SUBNET> using residual_256 = residual<256, SUBNET>;
+        template <typename SUBNET> using residual_512 = residual<512, SUBNET>;
+        template <typename SUBNET> using residual_1024 = residual<1024, SUBNET>;
+
+        template <typename INPUT>
+        using backbone19 = 
+            block5<1024,
+            max_pool<2, 2, 2, 2,
+            block5<512,
+            max_pool<2, 2, 2, 2,
+            block3<256,
+            max_pool<2, 2, 2, 2,
+            block3<128,
+            max_pool<2, 2, 2, 2,
+            convolutional<64, 3, 1, 1,
+            max_pool<2, 2, 2, 2,
+            convolutional<32, 3, 1, 1,
+            INPUT>>>>>>>>>>>;
+
+        template <typename INPUT>
+        using backbone53 =
+            repeat<4, residual_1024,
+            convolutional<1024, 3, 2, 1,
+            repeat<8, residual_512,
+            convolutional<512, 3, 2, 1,
+            repeat<8, residual_256,
+            convolutional<256, 3, 2, 1,
+            repeat<2, residual_128,
+            convolutional<128, 3, 2, 1,
+            residual<64,
+            convolutional<64, 3, 2, 1,
+            convolutional<32, 3, 1, 1,
+            INPUT>>>>>>>>>>>;
+
+        using classifier19_type = loss_multiclass_log<
+            con<1000, 1, 1, 1, 1, avg_pool_everything<
+            backbone19<input_rgb_image>>>>;
+
+        using classifier53_type = loss_multiclass_log<
+            con<1000, 1, 1, 1, 1, avg_pool_everything<
+            backbone53<input_rgb_image>>>>;
+
+        using detector19_type = loss_yolo<
+            con<1, 1, 1, 1, 1,
+            backbone19<input_rgb_image>>>;
+
+        using detector53_type = loss_yolo<
+            con<1, 1, 1, 1, 1,
+            backbone53<input_rgb_image>>>;
+    };
+
+    using classifier19_train = def<bn_con, mish>::classifier19_type;
+    using classifier19_infer = def<affine, mish>::classifier19_type;
+    using detector19_train = def<bn_con, mish>::detector19_type;
+    using detector19_infer = def<affine, mish>::detector19_type;
+
+    using classifier53_train = def<bn_con, mish>::classifier53_type;
+    using classifier53_infer = def<affine, mish>::classifier53_type;
+    using detector53_train = def<bn_con, mish>::detector53_type;
+    using detector53_infer = def<affine, mish>::detector53_type;
+    // clang-format on
+
+}  // namespace darknet
+
+#endif  // darknet_h_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
+#include "darknet.h"
 #include "yolo.h"
-#include "resnet_backbone.h"
+// #include "resnet_backbone.h"
 
-#include <iostream>
 #include <dlib/data_io.h>
+#include <iostream>
 #include <resnet.h>
 
 #undef VISUAL
+
 #include <dlib/gui_widgets.h>
 
-namespace detector
-{
-    using namespace dlib;
-    template <template <typename> class BN, template <typename> class ACT>
-    using net_type = loss_yolo<
-        con<1, 1, 1, 1, 1,
-        typename resnet_backbone::def<BN, ACT>::template backbone_50<
-        input_rgb_image
-    >>>;
+// namespace detector
+// {
+//     using namespace dlib;
+//     template <template <typename> class BN, template <typename> class ACT>
+//     using net_type = loss_yolo<
+//         con<1, 1, 1, 1, 1,
+//         typename resnet_backbone::def<BN, ACT>::template backbone_50<
+//         input_rgb_image
+//     >>>;
 
-    using train = net_type<bn_con, mish>;
-    using infer = net_type<affine, mish>;
-}
+//     using train = net_type<bn_con, mish>;
+//     using infer = net_type<affine, mish>;
+// }
 
 struct training_sample
 {
@@ -35,21 +37,39 @@ try
     if (argc > 1)
     {
         dlib::image_window win;
-        detector::train net;
-        dlib::deserialize("./yolo-resnet50-backbone.dnn") >> net;
+        // detector::train net;
+        // darknet::train net;
+        darknet::detector19_infer net;
+        {
+            darknet::detector19_infer temp;
+            auto trainer = dlib::dnn_trainer(temp);
+            trainer.set_synchronization_file("yolo_darknet19_sync");
+            temp = trainer.get_net(dlib::force_flush_to_disk::no);
+            net = temp;
+        }
+
+        // dlib::deserialize("./yolo-resnet50-backbone.dnn") >> net;
+        // dlib::deserialize("./yolo-darknet53-backbone.dnn") >> net;
         std::cout << net << '\n';
         dlib::matrix<dlib::rgb_pixel> image, input_image;
         for (int i = 1; i < argc; ++i)
         {
             dlib::load_image(image, argv[i]);
-            dlib::scale_aspect_ratio(image, net.loss_details().get_options().get_input_size(), input_image);
+            dlib::letter_box(
+                image,
+                net.loss_details().get_options().get_input_size(),
+                input_image);
             win.set_image(input_image);
             const auto detections = net.process(input_image, -1);
             std::cout << "found " << detections.size() << " detections\n";
             for (const auto& det : detections)
             {
-                std::cout << det.label << ", " << det.detection_confidence << ", " << det.rect << '\n';
-                win.add_overlay(det.rect, dlib::rgb_pixel(0, 255, 0), det.label);
+                std::cout << det.label << ", " << det.detection_confidence << ", " << det.rect
+                          << '\n';
+                win.add_overlay(
+                    det.rect,
+                    dlib::rgb_pixel(255, 0, 0),
+                    det.label + ": " + std::to_string(det.detection_confidence));
             }
             std::cin.get();
             win.clear_overlay();
@@ -59,15 +79,22 @@ try
     {
         std::vector<dlib::matrix<dlib::rgb_pixel>> images;
         std::vector<std::vector<dlib::mmod_rect>> bboxes;
-        dlib::load_image_dataset(images, bboxes, "./pascal.xml");
+        // dlib::load_image_dataset(images, bboxes, "./pascal.xml");
+        dlib::load_image_dataset(images, bboxes, "./sunglasses.xml");
         std::cout << "image dataset loaded: " << images.size() << " images\n";
 
-        dlib::yolo_options options(608, 32, bboxes);
-        detector::train net(options);
+        const long input_size = 448;
+        dlib::yolo_options options(input_size, 32, bboxes);
+        darknet::detector19_train net(options);
         net.subnet().layer_details().set_num_filters(options.get_labels().size() + 5);
-        dlib::deserialize("resnet50_pretrained_backbone.dnn") >> net.subnet().subnet();
-        dlib::set_all_learning_rate_multipliers(net.subnet().subnet(), 0.01);
-
+        // dlib::deserialize("resnet50_pretrained_backbone.dnn") >> net.subnet().subnet();
+        // dlib::set_all_learning_rate_multipliers(net.subnet().subnet(), 0.01);
+        {
+            dlib::matrix<dlib::rgb_pixel> dummy(input_size, input_size);
+            net(dummy);
+        }
+        std::cout << net << '\n';
+        std::cout << "num parameters: " << dlib::count_parameters(net) << '\n';
         dlib::pipe<training_sample> training_data(1000);
 
 #ifdef VISUAL
@@ -79,39 +106,45 @@ try
         {
             dlib::rand rnd(time(nullptr) + seed);
 
-            const long min_size = options.get_downsampling_factor() * 1.5;
-            const long chip_dim = options.get_input_size();
-            dlib::random_cropper cropper;
-            cropper.set_seed(time(nullptr) + seed);
-            cropper.set_chip_dims(chip_dim, chip_dim);
-            cropper.set_translate_amount(0.1);
-            cropper.set_background_crops_fraction(0);
-            cropper.set_randomly_flip(true);
-            cropper.set_max_rotation_degrees(30);
-            cropper.set_min_object_size(min_size, min_size);
-            cropper.set_max_object_size(0.9);
+            // const long min_size = options.get_downsampling_factor() * 1.5;
+            // const long chip_dim = options.get_input_size();
+            // dlib::random_cropper cropper;
+            // cropper.set_seed(time(nullptr) + seed);
+            // cropper.set_chip_dims(chip_dim, chip_dim);
+            // cropper.set_translate_amount(0.1);
+            // cropper.set_background_crops_fraction(0);
+            // cropper.set_randomly_flip(true);
+            // cropper.set_max_rotation_degrees(0);
+            // cropper.set_min_object_size(min_size, min_size);
+            // cropper.set_max_object_size(0.9);
 
             training_sample sample;
             while (training_data.is_enabled())
             {
                 const auto idx = rnd.get_random_32bit_number() % images.size();
-                const auto& image = images[idx];
-                const auto& boxes = bboxes[idx];
-                cropper(image, boxes, sample.image, sample.boxes);
-                const auto temp = options.generate_map(sample.image, sample.boxes);
-                sample.image = temp.first;
-                sample.yolo_map = temp.second;
+                // const auto& image = images[idx];
+                // const auto& boxes = bboxes[idx];
+                // cropper(image, boxes, sample.image, sample.boxes);
+
+                const auto temp = options.generate_map(images[idx], bboxes[idx]);
+                sample.image = std::move(temp.first);
+                sample.yolo_map = std::move(temp.second);
                 dlib::disturb_colors(sample.image, rnd);
 
 #ifdef VISUAL
+                dlib::matrix<dlib::rgb_pixel> square_image;
+                const auto [scale, offset] =
+                    dlib::letter_box(images[idx], options.get_input_size(), square_image);
                 win.set_image(sample.image);
-                for (const auto& box : sample.boxes)
+                for (const auto& box : bboxes[idx])
                 {
-                    win.add_overlay(box.rect, dlib::rgb_pixel(0, 255, 0), box.label);
+                    const auto s_rect = dlib::scale_rect(box.rect, scale);
+                    sample.boxes.push_back(dlib::translate_rect(s_rect, offset));
+                    win.add_overlay(sample.boxes.back(), dlib::rgb_pixel(0, 255, 0), box.label);
                 }
                 std::cin.get();
                 win.clear_overlay();
-                win.set_image(options.overlay_map(sample.image, sample.boxes));
+                win.set_image(options.overlay_map(images[idx], bboxes[idx]));
                 std::cin.get();
 #endif
 
@@ -122,18 +155,19 @@ try
         std::vector<std::thread> data_loaders;
         for (int i = 0; i < 1; ++i)
         {
-            data_loaders.emplace_back([&data_loader, i](){ data_loader(i + 1); });
+            data_loaders.emplace_back([&data_loader, i]() { data_loader(i + 1); });
         }
 
         auto trainer = dlib::dnn_trainer(net);
         trainer.be_verbose();
-        trainer.set_learning_rate(0.01);
-        trainer.set_iterations_without_progress_threshold(10000);
-        trainer.set_mini_batch_size(4);
+        trainer.set_learning_rate(0.1);
+        trainer.set_iterations_without_progress_threshold(3000);
+        trainer.set_mini_batch_size(8);
+        trainer.set_synchronization_file("yolo_darknet19_sync", std::chrono::minutes(10));
         std::cout << trainer << '\n';
         std::vector<dlib::matrix<dlib::rgb_pixel>> minibatch_images;
         std::vector<dlib::yolo_options::map_type> minibatch_labels;
-        while(trainer.get_learning_rate() > 1e-6)
+        while (trainer.get_learning_rate() > 1e-6)
         {
             minibatch_images.clear();
             minibatch_labels.clear();
@@ -154,7 +188,7 @@ try
         }
         trainer.get_net();
         net.clean();
-        dlib::serialize("yolo-resnet50-backbone.dnn") << net;
+        dlib::serialize("yolo-darknet19.dnn") << net;
     }
 }
 catch (const std::exception& e)
