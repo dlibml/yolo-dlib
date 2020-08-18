3e2af6aefe24e4bf2db7887fb860dca83e49f984
Adrià Arrufat <adria.arrufat@outlook.com>
2020-08-18 17:32:40 +0900

    fix formatting again...

--- src/yolo.h
+++ src/yolo.h
@@ -9,7 +9,8 @@ namespace dlib
     std::pair<double, point> letter_box(
         const matrix<rgb_pixel>& image,
         const long size,
-        matrix<rgb_pixel>& square_image)
+        matrix<rgb_pixel>& square_image
+    )
     /*!
         requires
             - size > 0
@@ -69,13 +70,12 @@ namespace dlib
         yolo_options(
             const long input_size_,
             const long downsampling_factor_,
-            const std::vector<std::vector<mmod_rect>>& boxes)
-            : input_size(input_size_),
-              downsampling_factor(downsampling_factor_)
+            const std::vector<std::vector<mmod_rect>>& boxes
+        ) :
+            input_size(input_size_),
+            downsampling_factor(downsampling_factor_)
         {
-            DLIB_CASSERT(
-                input_size % downsampling_factor == 0,
-                "input_size is not a multiple of downsampling_factor");
+            DLIB_CASSERT(input_size % downsampling_factor == 0, "input_size is not a multiple of downsampling_factor");
             std::set<std::string> labels_set;
             for (const auto& v : boxes)
             {
@@ -102,8 +102,10 @@ namespace dlib
         const std::vector<std::string>& get_labels() const { return labels; }
 
         // Generates a YOLO truth map from an image and its set of bounding boxes
-        std::pair<matrix<rgb_pixel>, map_type>
-            generate_map(const matrix<rgb_pixel>& image, const std::vector<mmod_rect>& boxes)
+        std::pair<matrix<rgb_pixel>, map_type> generate_map(
+            const matrix<rgb_pixel>& image,
+            const std::vector<mmod_rect>& boxes
+        )
         {
             const long grid_size = get_grid_size();
             map_type label_map;
@@ -138,18 +140,13 @@ namespace dlib
                     // the x offset from the top left corner
                     const auto offset_x = static_cast<float>(oc.x()) / downsampling_factor - c;
                     // the class index
-                    const size_t idx =
-                        std::find(labels.begin(), labels.end(), boxes[i].label) - labels.begin();
+                    const size_t idx = std::find(labels.begin(), labels.end(), boxes[i].label) - labels.begin();
                     if (idx < labels.size())
                     {
                         label_map[0](r, c) = 1.f;
-                        DLIB_CASSERT(
-                            0 <= offset_x && offset_x <= 1,
-                            "wrong offset_x: " << offset_x);
+                        DLIB_CASSERT(0 <= offset_x && offset_x <= 1, "wrong offset_x: " << offset_x);
                         label_map[1](r, c) = offset_x;
-                        DLIB_CASSERT(
-                            0 <= offset_y && offset_y <= 1,
-                            "wrong offset_y: " << offset_y);
+                        DLIB_CASSERT(0 <= offset_y && offset_y <= 1, "wrong offset_y: " << offset_y);
                         label_map[2](r, c) = offset_y;
                         label_map[3](r, c) = std::sqrt(height > 1 ? 1 : height);
                         label_map[4](r, c) = std::sqrt(width > 1 ? 1 : width);
@@ -160,8 +157,10 @@ namespace dlib
             return std::make_pair(resized_image, label_map);
         }
 
-        matrix<rgb_pixel>
-            overlay_map(const matrix<rgb_pixel>& image, const std::vector<mmod_rect>& boxes)
+        matrix<rgb_pixel> overlay_map(
+            const matrix<rgb_pixel>& image,
+            const std::vector<mmod_rect>& boxes
+        )
         {
             auto [resized_image, yolo_map] = generate_map(image, boxes);
             for (long r = 0; r < get_grid_size(); ++r)
@@ -171,9 +170,7 @@ namespace dlib
 
                     if (yolo_map[0](r, c) == 1)
                     {
-                        point center(
-                            (c + yolo_map[1](r, c)) * downsampling_factor,
-                            (r + yolo_map[2](r, c)) * downsampling_factor);
+                        point center((c + yolo_map[1](r, c)) * downsampling_factor, (r + yolo_map[2](r, c)) * downsampling_factor);
                         draw_solid_circle(resized_image, center, 3, rgb_pixel(255, 255, 255));
                         draw_solid_circle(resized_image, center, 2, rgb_pixel(0, 0, 0));
                         // paint the grid
@@ -182,11 +179,7 @@ namespace dlib
                             for (long gc = 0; gc < downsampling_factor; ++gc)
                             {
                                 rgb_alpha_pixel p(0, 255, 0, 64);
-                                assign_pixel(
-                                    resized_image(
-                                        r * downsampling_factor + gr,
-                                        c * downsampling_factor + gc),
-                                    p);
+                                assign_pixel(resized_image(r * downsampling_factor + gr, c * downsampling_factor + gc), p);
                             }
                         }
                         // bounding boxes
@@ -194,11 +187,7 @@ namespace dlib
                         const long w = std::pow(yolo_map[4](r, c), 2) * input_size;
                         draw_rectangle(
                             resized_image,
-                            rectangle(
-                                center.x() - w / 2,
-                                center.y() - h / 2,
-                                center.x() + w / 2,
-                                center.y() + h / 2),
+                            rectangle(center.x() - w / 2, center.y() - h / 2, center.x() + w / 2, center.y() + h / 2),
                             rgb_pixel(255, 0, 0),
                             1);
                     }
@@ -220,16 +209,14 @@ namespace dlib
         friend inline void serialize(const yolo_options& item, std::ostream& out);
         friend inline void deserialize(yolo_options& item, std::istream& in);
 
-        private:
+    private:
         long input_size;
         long downsampling_factor;
         std::vector<std::string> labels;
 
         std::pair<double, double> get_row_col_scales(const long nr, const long nc)
         {
-            return std::make_pair(
-                static_cast<double>(input_size) / nr,
-                static_cast<double>(input_size) / nc);
+            return std::make_pair(static_cast<double>(input_size) / nr, static_cast<double>(input_size) / nc);
         }
     };
 
@@ -247,14 +234,11 @@ namespace dlib
         int version = 0;
         deserialize(version, in);
         if (version != 0)
-            throw serialization_error(
-                "Unexpected version found with deserializing dlib::yolo_options");
+            throw serialization_error("Unexpected version found with deserializing dlib::yolo_options");
         deserialize(item.input_size, in);
         deserialize(item.downsampling_factor, in);
         deserialize(item.labels, in);
-        DLIB_CASSERT(
-            item.input_size % item.downsampling_factor == 0,
-            "input_size is not a multiple of downsampling_factor");
+        DLIB_CASSERT(item.input_size % item.downsampling_factor == 0, "input_size is not a multiple of downsampling_factor");
     }
 
     bool operator<(const mmod_rect& a, const mmod_rect& b)
@@ -264,7 +248,7 @@ namespace dlib
 
     class loss_yolo_
     {
-        public:
+    public:
         typedef yolo_options::map_type training_label_type;
         typedef std::vector<mmod_rect> output_label_type;
 
@@ -274,18 +258,18 @@ namespace dlib
 
         const yolo_options& get_options() const { return options; };
 
-        template <typename SUB_TYPE, typename label_iterator> void to_label(
+        template <typename SUB_TYPE, typename label_iterator>
+        void to_label(
             const tensor& input_tensor,
             const SUB_TYPE& sub,
             label_iterator iter,
-            const double adjust_threshold = 0) const
+            const double adjust_threshold = 0
+        ) const
         {
             const tensor& output_tensor = sub.get_output();
             DLIB_CASSERT(sub.sample_expansion_factor() == 1, "expansion factor should be 1");
             DLIB_CASSERT(output_tensor.k() >= 6, "YOLO layer requires at least 6 channels");
-            DLIB_CASSERT(
-                input_tensor.num_samples() == output_tensor.num_samples(),
-                "num_samples mismatch");
+            DLIB_CASSERT(input_tensor.num_samples() == output_tensor.num_samples(), "num_samples mismatch");
 
             const float* const out_data = output_tensor.host();
 
@@ -299,20 +283,15 @@ namespace dlib
                         const float objectness = out_data[tensor_index(output_tensor, i, 0, r, c)];
                         if (objectness > adjust_threshold)
                         {
-                            const float offset_x =
-                                out_data[tensor_index(output_tensor, i, 1, r, c)];
-                            const float offset_y =
-                                out_data[tensor_index(output_tensor, i, 2, r, c)];
-                            const float height =
-                                std::pow(out_data[tensor_index(output_tensor, i, 3, r, c)], 2);
-                            const float width =
-                                std::pow(out_data[tensor_index(output_tensor, i, 4, r, c)], 2);
+                            const float offset_x = out_data[tensor_index(output_tensor, i, 1, r, c)];
+                            const float offset_y = out_data[tensor_index(output_tensor, i, 2, r, c)];
+                            const float height = std::pow(out_data[tensor_index(output_tensor, i, 3, r, c)], 2);
+                            const float width = std::pow(out_data[tensor_index(output_tensor, i, 4, r, c)], 2);
                             float max_value = out_data[tensor_index(output_tensor, i, 5, r, c)];
                             long label_idx = 0;
                             for (long k = 6; k < output_tensor.k(); ++k)
                             {
-                                const float value =
-                                    out_data[tensor_index(output_tensor, i, k, r, c)];
+                                const float value = out_data[tensor_index(output_tensor, i, k, r, c)];
                                 if (value > max_value)
                                 {
                                     max_value = value;
@@ -339,11 +318,15 @@ namespace dlib
             }
         }
 
-        template <typename const_label_iterator, typename SUBNET>
-        double compute_loss_value_and_gradient(
+        template <
+            typename const_label_iterator,
+            typename SUBNET
+            >
+        double compute_loss_value_and_gradient (
             const tensor& input_tensor,
             const const_label_iterator truth,
-            SUBNET& sub) const
+            SUBNET& sub
+        ) const
         {
             const tensor& output_tensor = sub.get_output();
             tensor& grad = sub.get_gradient_input();
@@ -352,54 +335,33 @@ namespace dlib
             DLIB_CASSERT(input_tensor.num_samples() != 0, "we need at least 1 training sample");
             DLIB_CASSERT(input_tensor.num_samples() == grad.num_samples(), "num_samples mismatch");
             DLIB_CASSERT(output_tensor.k() >= 6, "YOLO layer requires at least 6 channels");
-            DLIB_CASSERT(
-                output_tensor.nr() == options.get_grid_size(),
-                "wrong output nr: " << output_tensor.nr());
-            DLIB_CASSERT(
-                output_tensor.nc() == options.get_grid_size(),
-                "wrong output nc: " << output_tensor.nc());
-            DLIB_CASSERT(
-                output_tensor.nr() == grad.nr() && output_tensor.nc() == grad.nc() &&
-                    output_tensor.k() == grad.k(),
-                "grad shape mismatch");
+            DLIB_CASSERT(output_tensor.nr() == options.get_grid_size(), "wrong output nr: " << output_tensor.nr());
+            DLIB_CASSERT(output_tensor.nc() == options.get_grid_size(), "wrong output nc: " << output_tensor.nc());
+            DLIB_CASSERT(output_tensor.nr() == grad.nr() &&
+                         output_tensor.nc() == grad.nc() &&
+                         output_tensor.k() == grad.k(), "grad shape mismatch");
 
             for (long i = 0; i < output_tensor.num_samples(); ++i)
             {
                 const_label_iterator truth_map_ptr = (truth + i);
-                DLIB_CASSERT(
-                    truth_map_ptr->at(0).nr() == output_tensor.nr() &&
-                        truth_map_ptr->at(0).nc() == output_tensor.nc(),
-                    "truth size = " << truth_map_ptr->at(0).nr() << " x "
-                                    << truth_map_ptr->at(0).nc() << ", output size = "
-                                    << output_tensor.nr() << " x " << output_tensor.nc());
+                DLIB_CASSERT(truth_map_ptr->at(0).nr() == output_tensor.nr() &&
+                             truth_map_ptr->at(0).nc() == output_tensor.nc(),
+                             "truth size = " << truth_map_ptr->at(0).nr() << " x " << truth_map_ptr->at(0).nc()
+                             << ", output size = " << output_tensor.nr() << " x " << output_tensor.nc());
             }
 
             const float* out_data = output_tensor.host();
             float* g = grad.host_write_only();
 
-            // The loss we output is the average loss of over the minibatch and also over each
-            // objectness element
-            const double scale =
-                1.0 / (output_tensor.num_samples() * output_tensor.nr() * output_tensor.nc());
+            // The loss we output is the average loss of over the minibatch and also over each objectness element
+            const double scale = 1.0 / (output_tensor.num_samples() * output_tensor.nr() * output_tensor.nc());
             const double lambda_noobj = 0.5;
             const double lambda_coord = 5.0;
 
             // extract the subtensors from the output tensor
-            resizable_tensor obj_tensor(
-                output_tensor.num_samples(),
-                1,
-                output_tensor.nr(),
-                output_tensor.nc());
-            resizable_tensor bbr_tensor(
-                output_tensor.num_samples(),
-                4,
-                output_tensor.nr(),
-                output_tensor.nc());
-            resizable_tensor cls_tensor(
-                output_tensor.num_samples(),
-                output_tensor.k() - 5,
-                output_tensor.nr(),
-                output_tensor.nc());
+            resizable_tensor obj_tensor(output_tensor.num_samples(), 1, output_tensor.nr(), output_tensor.nc());
+            resizable_tensor bbr_tensor(output_tensor.num_samples(), 4, output_tensor.nr(), output_tensor.nc());
+            resizable_tensor cls_tensor(output_tensor.num_samples(), output_tensor.k() - 5, output_tensor.nr(), output_tensor.nc());
             float* obj_data = obj_tensor.host();
             float* bbr_data = bbr_tensor.host();
             float* cls_data = cls_tensor.host();
@@ -439,23 +401,11 @@ namespace dlib
             double loss_bbr = 0;
             double loss_cls = 0;
 
-            resizable_tensor temp_obj_tensor(
-                obj_tensor.num_samples(),
-                obj_tensor.k(),
-                obj_tensor.nr(),
-                obj_tensor.nc());
+            resizable_tensor temp_obj_tensor(obj_tensor.num_samples(), obj_tensor.k(), obj_tensor.nr(), obj_tensor.nc());
             tt::sigmoid(temp_obj_tensor, obj_tensor);
-            resizable_tensor temp_bbr_tensor(
-                bbr_tensor.num_samples(),
-                bbr_tensor.k(),
-                bbr_tensor.nr(),
-                bbr_tensor.nc());
+            resizable_tensor temp_bbr_tensor(bbr_tensor.num_samples(), bbr_tensor.k(), bbr_tensor.nr(), bbr_tensor.nc());
             tt::sigmoid(temp_bbr_tensor, bbr_tensor);
-            resizable_tensor temp_cls_tensor(
-                cls_tensor.num_samples(),
-                cls_tensor.k(),
-                cls_tensor.nr(),
-                cls_tensor.nc());
+            resizable_tensor temp_cls_tensor(cls_tensor.num_samples(), cls_tensor.k(), cls_tensor.nr(), cls_tensor.nc());
             tt::softmax(temp_cls_tensor, cls_tensor);
 
             float* temp_obj_data = temp_obj_tensor.host();
@@ -514,9 +464,7 @@ namespace dlib
 
                         // category classifier (loss multiclass log per pixel)
                         const float y = (*truth)[5](r, c);
-                        DLIB_CASSERT(
-                            static_cast<long>(y) < cls_tensor.k(),
-                            "y: " << y << ", cls_tensor.k(): " << cls_tensor.k());
+                        DLIB_CASSERT(static_cast<long>(y) < cls_tensor.k(), "y: " << y << ", cls_tensor.k(): " << cls_tensor.k());
                         for (long k = 5; k < output_tensor.k(); ++k)
                         {
                             const size_t idx = tensor_index(output_tensor, i, k, r, c);
@@ -585,7 +533,7 @@ namespace dlib
 
         friend void to_xml(const loss_yolo_ /*item*/, std::ostream& out) { out << "<loss_yolo/>"; }
 
-        private:
+    private:
         yolo_options options;
 
         static size_t tensor_index(
